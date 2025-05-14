77eb632b4851a62b49f8c70cd6a642114c850ce3
Vladimir Aseev <nemesisv@mail.ru>
2025-05-15 01:11:08 +0200

    add attributes to flex panel

--- components/flexPanel/flexPanel.js
+++ components/flexPanel/flexPanel.js
@@ -3,11 +3,20 @@ import attributes from '../attributes/attributes.js';
 
 loader.loadCSS(import.meta.resolve('./flexPanel.css'));
 
-class FlexPanel extends HTMLElement {  
+class FlexPanel extends HTMLElement {
+  #direction = 'horizontal';
   constructor() {
     super();
     attributes.loadAttributes(this);
   }
+
+  set direction(value) {
+    this.classList.remove(this.#direction);
+    this.classList.add(value);
+    this.#direction = value;
+  }
+
+  get direction() { return this.#direction; }
 }
 
 customElements.define('intention-flex-panel', FlexPanel);
