774a90ab58bff58919fa4010816f7020bd5f4d19
Vladimir Aseev <nemesisv@mail.ru>
2025-05-17 13:27:37 +0200

    fix

--- components/core/title/title.js
+++ components/core/title/title.js
@@ -14,7 +14,7 @@ class Title extends Container {
   get size() { return this._size; }
   
   async render() {
-    const body = this.firstChild.innerHTML;
+    const body = this.firstChild?.innerHTML || this.innerHTML;
     this.innerHTML = `
       <h${this.size}>${body}</h${this.size}>
     `;
