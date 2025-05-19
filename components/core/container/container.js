5dfc870979233231c96ea0414f714f40768ad8b6
Vladimir Aseev <nemesisv@mail.ru>
2025-05-20 01:54:16 +0200

    fix

--- components/core/container/container.js
+++ components/core/container/container.js
@@ -1,6 +1,5 @@
 import attributes from '../attributes/attributes.js';
 import dom from '../dom/dom.js';
-import { GridPanel } from '../gridPanel/gridPanel.js';
 
 export class Container extends HTMLElement {  
   #template = null;
