3c759e50f3e0a168198af16dc29aaaf686c0ad27
Vladimir Aseev <nemesisv@mail.ru>
2025-05-20 04:16:31 +0200

    fix

--- components/core/componentRoot/componentRoot.js
+++ components/core/componentRoot/componentRoot.js
@@ -1,5 +1,6 @@
 import dom from '../dom/dom.js';
 import { Container } from '../container/container.js';
+import string from '../string/string.js';
 
 export class ComponentRoot extends Container {
   #name = null;
