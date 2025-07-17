208705afb187694425fe14641a22f2b59f527dc4
Vladimir Aseev <nemesisv@mail.ru>
2025-07-17 11:13:39 +0200

    fix

--- current/themes/dashboard/application/application.js
+++
@@ -1,8 +0,0 @@
-import loader from '../../../core/loader/loader.js';
-import { Application } from '../../../core/application/application.js';
-
-loader.loadCSS(import.meta.resolve('./application.css'));
-
-export class DashboardApplication extends Application { }
-
-customElements.define('i-dashboard-application', DashboardApplication);
